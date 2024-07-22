45a9b07424b4d6bd8e3fd5bec97bc7fde7783686
Koovendhan Elango <kovendhanelango@gmail.com>
2024-07-22 17:25:46 -0400

    Update CSS

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", function() {
     const timelineItemsLeft = document.querySelectorAll(".timeline-left .timeline-item");
     const timelineItemsRight = document.querySelectorAll(".timeline-right .timeline-item");
